271539276a033ba74f7f6afccf3582de12681552
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-29 13:27:00 +0600

    Data tyeps

---
+++ datatyeps.js
@@ -0,0 +1,32 @@
+// Primitive Data
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+
+// Non Primitive Data
+// 1. Array
+// 2. Object
+// 3. Function
+
+// String
+const person = 'Nahian';
+console.log(typeof person)
+
+// Nubmer
+const no = 50;
+console.log(typeof no)
+
+// Boolean
+const istrue =true;
+console.log(typeof istrue)
+
+// Null
+const test = null;
+console.log(typeof test);
+
+// Undefined
+let another;
+console.log(another);
+
