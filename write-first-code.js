b6920653a6edc8b37807af7539e4c3c6d785e276
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-29 13:08:13 +0600

    Var, Let & Const

---
+++ write-first-code.js
@@ -0,0 +1,18 @@
+// var
+// var uname = 'Nahian'
+// console.log(uname)
+// uname = 'Another';
+// console.log(uname)
+
+// let
+
+// let uname = 'Let Nahian'
+// console.log(uname);
+// uname = 'Let Another'
+// console.log(uname);
+
+// const
+
+// const age = 'ten';
+// age = 'twelve';
+// console.log(age);
